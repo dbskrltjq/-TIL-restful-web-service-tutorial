28fb3df4bfbb3594e0ea871a7a5dc96f8a652505
youna <dbskrltjq@gmail.com>
2022-11-07 17:56:14 +0900

    first upload

---
+++ src/main/java/kr/co/restfulwebservice/RestfulWebServiceTutorialApplication.java
@@ -0,0 +1,13 @@
+package kr.co.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/restfulwebservice/RestfulWebServiceTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
