adb6e829574febe1010ed242ea60c252b5db8366
youna <dbskrltjq@gmail.com>
2022-11-08 16:43:06 +0900

    DELETE, PUT METHOD 사용하기

--- src/main/java/kr/co/restfulwebservice/user/UserDaoService.java
+++ src/main/java/kr/co/restfulwebservice/user/UserDaoService.java
@@ -2,6 +2,7 @@ package kr.co.restfulwebservice.user;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -28,6 +29,33 @@ public class UserDaoService {
 				return user;
 			}
 		}
+		return null;
+	}
+	
+	public User deleteById(int id) {
+		Iterator<User> iterator = users.iterator(); 
+		
+		while(iterator.hasNext()) {	// 순차적으로 하나씩의 데이터를 가져온다.
+			User user = iterator.next();
+			
+			if(user.getId() == id) {
+				iterator.remove();
+				return user;
+			}
+		}
+		
+		return null;		// 해당 id가 존재하지 않는 경우
+	}
+	
+	public User updateUser(User user) {
+		User existUser = findOne(user.getId());
+		
+		if(existUser != null) {
+			existUser.setName(user.getName());
+			existUser.setJoinDate(user.getJoinDate());
+			return user;
+		}
+		
 		return null;
 	}
 	
